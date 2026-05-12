4972575084867f87720d11b0d0ba440681ffc466
vilafloresta2-creator <vilafloresta2@gmail.com>
2026-05-12 17:30:56 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 console.log("JS carregado");
 const tela = document.getElementById("tela");
-const API = "https://script.google.com/macros/s/AKfycbx28WB6HlWGyC2Larm3DgF3n1DMYH5VfEWvwyNYCSxmv2AJy1MWWZAyM00UAosLeqq8/exec";
 
 let associados = [];
 let cobrancas = [];
